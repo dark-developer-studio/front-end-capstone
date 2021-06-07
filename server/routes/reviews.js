80c8200cb62a8be43e5c0ef706ee180ac01eb904
mhhutton <mhhutton@gmail.com>
2021-06-07 15:46:23 -0600

    Updates reviews route by creating a get request from reviews api using GETHUB API KEY. Query params can be used to get specific response data.

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,7 +1,34 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/', (req, res) => {});
+const axios = require('axios');
+
+const { GITHUB_API_KEY } = require('../../config.js');
+
+router.get('/revs', (req, res) => {
+  axios
+  .get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/reviews', {
+    headers: {
+      Authorization: GITHUB_API_KEY
+      //---------ADDITIONAL HEADERS INCASE NEEDED
+      // Accept: 'application/json',
+      // 'User-Agent': 'request',
+    },
+    params: {
+      product_id: req.query.product_id,
+      count: req.query.count,
+      page : req.query.page,
+      sort: req.query.sort
+    }
+  })
+  .then( (response) => response.data)
+  .then( (data) => {
+    res.send(data);
+  })
+  .catch( (err) => {
+    console.log(err);
+    res.send(err);
+  })
+});
 
 module.exports.router = router;
