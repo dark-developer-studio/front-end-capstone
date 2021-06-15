1bd60206adb6a826d5d8a36af88acbd6c05b0ddb
mhhutton <mhhutton@gmail.com>
2021-06-15 06:07:59 -0600

    Updates with work in progress post route

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -37,6 +37,32 @@ router.get('/revs', (req, res) => {
     });
 });
 
+router.post('/reviews', (req, res) => {
+  axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/reviews', {
+    product_id: req.body.product_id,
+    rating: req.body.rating,
+    summary: req.body.summary,
+    body: req.body.body,
+    recommend: req.body.recommend,
+    name: req.body.name,
+    email: req.body.email,
+    photos: req.body.photos,
+    characteristics: req.body.characteristics
+  }, {
+    headers: {
+      Authorization: GITHUB_API_KEY
+    }
+  })
+    .then((response) => {
+      res.status(201);
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
+
 router.get('/meta', (req, res) => {
   axios
     .get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/reviews/meta', {
