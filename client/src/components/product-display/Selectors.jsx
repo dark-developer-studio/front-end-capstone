aea3c134e0c2a2ed3d5c5243b523e25ae99b6510
James Moore <jameswmoore99@gmail.com>
2021-06-17 15:05:45 -0600

    Drop down bug fixes and refactoring of code

--- client/src/components/product-display/Selectors.jsx
+++ client/src/components/product-display/Selectors.jsx
@@ -105,6 +105,7 @@ const Selectors = (props) => {
           handleSizeChange={handleSizeChange}
           setSizeForQuantity={setSizeForQuantity}
           selectSizeValue={selectSizeValue}
+          setSelectQuantityValue={setSelectQuantityValue}
           open={open}
           handleClickAway={handleClickAway}
           setSkusId={setSkusId}
@@ -129,9 +130,9 @@ const Selectors = (props) => {
           className={classes.button}
           variant="outlined"
           onClick={(event) => {
-            if (sizeForQuantity === '' && quantityArr.length === 0) {
+            if (sizeForQuantity === '' || quantityArr.length === 0) {
               handlePopoverSizeOpen(event);
-            } else if (sizeForQuantity !== '' && quantitiySelected === '') {
+            } else if (sizeForQuantity !== '' && (quantitiySelected === undefined || quantitiySelected === '')) {
               handlePopoverQuantityOpen(event);
             }
             if (sizeForQuantity !== '' && quantitiySelected !== '') {
