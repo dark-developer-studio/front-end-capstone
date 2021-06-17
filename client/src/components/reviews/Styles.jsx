af6fd3ef727a2f33e6727959d1441d5da913626c
mhhutton <mhhutton@gmail.com>
2021-06-16 22:57:22 -0600

    Updates and adds styles for review tile window

--- client/src/components/reviews/Styles.jsx
+++ client/src/components/reviews/Styles.jsx
@@ -9,46 +9,47 @@ const useStyles = makeStyles(() => ({
     alignContent: 'center'
   },
   topGrid: {
-    // border: "3px solid red",
     marginTop: '10px'
-    // alignContent: "center"
-    // justify:"space-evenly",
-    // alignContent:"center"
   },
   componentTitle: {
-    // border: "3px solid blue",
     display: 'flex',
     padding: '10px',
     fontSize: '30px'
   },
   totalRev: {
-    // border: "3px solid blue",
     display: 'flex',
     padding: '10px'
   },
   sortby: {
-    // border: "3px solid blue",
     display: 'flex',
     padding: '10px'
   },
   midGrid: {
-    // border: "3px solid red",
     display: 'flex',
     margin: '10px 0 10px 0'
   },
   leftPanel: {
-    // border: "3px solid green",
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignContent: 'center'
   },
   rightPanel: {
-    // border: "3px solid green",
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignContent: 'center'
+  },
+  ReviewTileViewPort: {
+    border: '1px solid black',
+    padding: '3px',
+    margin: '15px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+    maxHeight: '85vh',
+    overflow: 'auto'
   },
   reviewTile: {
     border: '1px solid black',
@@ -57,19 +58,17 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'space-around',
     marginTop: '10px',
     marginBottom: '10px',
-    padding: '10px'
+    padding: '10px',
+    margin: '5px'
   },
   buttonContainer: {
-    // border: "3px solid orange",
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-around'
   },
   moreRevBtn: {
-    // border: "3px solid red"
   },
   addRevBtn: {
-    // border: "3px solid blue"
   }
 }));
 
