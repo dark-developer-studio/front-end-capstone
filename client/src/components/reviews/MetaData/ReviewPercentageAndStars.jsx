d94833a1c7885f4c0466ac9734e8019bb1f8d846
mhhutton <mhhutton@gmail.com>
2021-06-17 21:28:35 -0600

    seperates Ratings into its own component

--- client/src/components/reviews/MetaData/ReviewPercentageAndStars.jsx
+++ client/src/components/reviews/MetaData/ReviewPercentageAndStars.jsx
@@ -1,28 +1,24 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Rating from '@material-ui/lab/Rating';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 // helper functions
 import { calcAvgRating, calcStarRating } from '../helperFunctions.jsx';
 
 import { ReviewsContext } from '../../../helpers/context';
+import StarRating from './StarRating.jsx';
 
 const useStyles = makeStyles(() => ({
   parentContainer: {
-    // border: "3px solid red",
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center'
   },
   avgRating: {
-    // border: "3px solid green",
     fontSize: 'xxx-large',
     padding: '10px'
   },
   reviewStarRating: {
-    // border: "3px solid green",
     padding: '10px'
   }
 }));
@@ -30,7 +26,6 @@ const useStyles = makeStyles(() => ({
 const ReviewPercentageAndStars = () => {
   const { revsMetaData } = useContext(ReviewsContext);
   const [avgRating, setAvgRating] = useState(0);
-  const [revStarRating, setRevStarRating] = useState(0);
 
   const getAvgRating = () => {
     if (Object.keys(revsMetaData).length !== 0) {
@@ -45,24 +40,15 @@ const ReviewPercentageAndStars = () => {
     getAvgRating();
   }, [revsMetaData]);
 
-  useEffect(() => {
-    setRevStarRating(calcStarRating(avgRating));
-  }, [avgRating]);
+  // useEffect(() => {
+  //   setRevStarRating(calcStarRating(avgRating));
+  // }, [avgRating]);
 
   const classes = useStyles();
   return (
-    <div className={classes.parentContainer}>
+    <div className={classes.parentContainer} id="starsAndPercent">
       <div className={classes.avgRating}>{avgRating.toFixed(1)}</div>
-      <Rating
-        name="reviewStarRating"
-        className={classes.reviewStarRating}
-        readOnly={true}
-        size="large"
-        defaultValue={0}
-        precision={0.1}
-        value={revStarRating}
-        emptyIcon={<StarBorderIcon fontSize="inherit" />}
-      />
+      <StarRating avgRating={avgRating} />
     </div>
   );
 };
